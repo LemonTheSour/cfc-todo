c67e10031f56aaf6fe8240424997f7ae5494b297
Michael <m.m.downing@bigpond.com>
2022-11-26 09:04:35 +0800

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header-test",
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px;
+}
+</style>
